fe34825e6200605d2ee340539025e48a35e4e369
Pascal Hiller <pascal.hiller@uni-oldenburg.de>
2025-02-25 14:45:46 +0100

    fix header and add schemeSelection

--- src/Upload.jsx
+++ src/Upload.jsx
@@ -1,36 +1,44 @@
 import Header from "./Header";
+import './css/Upload.css'
+import { useNavigate } from "react-router-dom";
 
 function Upload() {
+
+  const navigate = useNavigate();
+
     return (
       <div>
         <Header/>
-        <button
-          type="button"
-          className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        >
-          <svg
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 48 48"
-            aria-hidden="true"
-            className="mx-auto size-12 text-gray-400"
+      
+        <div className="upload-card">
+          <button
+            type="button"
+            className="relative block mt-15 mb-15 w-1/2 rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >
-            <path
-              d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-          <span className="mt-2 block text-sm font-semibold text-gray-900">Datei hochladen</span>
-        </button>
-        <button
-              type="button"
-              className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={() => navigate("/upload")}
-              >
-              Dateien durchsuchen
-            </button>
+            <svg
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 48 48"
+              aria-hidden="true"
+              className="mx-auto size-12 text-gray-400"
+            >
+              <path
+                d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <span className="mt-2 block text-sm font-semibold text-gray-900">Datei hochladen</span>
+          </button>
+          <button
+                type="button"
+                className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={() => navigate("../schemeSelection")}
+                >
+                Dateien durchsuchen
+              </button>
+        </div>
       </div>
     );
   }
