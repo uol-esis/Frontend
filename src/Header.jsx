192cc270d603eb2818da8367407255056e727727
Teetje Hillmann <hillmann@uni-bremen.de>
2025-03-11 10:33:03 +0100

    P-3

--- src/Header.jsx
+++ src/Header.jsx
@@ -61,7 +61,7 @@ export default function Header() {
 
   return (
     <header className="bg-gray-100 sticky top-0 z-50">
-      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-8"> 
         <div className="flex items-center gap-x-12">
           <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
