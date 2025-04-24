28bd9189f884c3e56aeb7363e995672b9d8b1ea9
Riphios <christian.janssen1@uni-oldenburg.de>
2025-04-24 12:27:07 +0200

    LandingPage überarbeitet

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,11 +9,11 @@ function Home() {
       <div className='flex flex-col justify-start w-[35vw] h-[70vh] mt-[5vh]'>
         <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
           <div className="px-4 py-5 sm:p-6">
-            <img style={{ height: "192px", width: "100%", objectFit: "contain" }} src="/DB.png" alt="Logo" />
+            <img style={{ height: "192px", width: "100%", objectFit: "contain" }} src="/UploadPic.png" alt="Logo" />
           </div>
           <div className="flex flex-col justify-start gap-4 p-4">
-            <p className='text-xl font-semibold'>Daten und Schemata</p>
-            <p>Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            <p className='text-xl font-semibold'>Daten hochladen</p>
+            <p>Excel Datein auswählen, anpassen und in die Datenbank hochladen.</p>
             <button
               type="button"
               className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -28,18 +28,18 @@ function Home() {
       <div className='flex flex-col justify-start w-[35vw] h-[70vh] mt-[5vh]'>
         <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
           <div className="px-4 py-5 sm:p-6">
-            <img style={{ height: "192px", width: "100%", objectFit: "contain" }} src="/Metabase.png" alt="Logo" />
+            <img style={{ height: "192px", width: "100%", objectFit: "contain" }} src="/DataVisPic.png" alt="Logo" />
           </div>
           <div className="flex flex-col justify-start gap-4 p-4">
-            <p className='text-xl font-semibold'>Metabase</p>
-            <p>Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            <p className='text-xl font-semibold'>Datenvisualisierung</p>
+            <p>Daten aus der Datenbank zu Diagrammen und Statistiken aufbereiten.</p>
             <button
               type="button"
               className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               onClick={() => window.location.href = import.meta.env.VITE_METABASE_ENDPOINT}
             >
               <div className="flex items-center justify-center g-3">
-                Metabase
+                Visualisierung
                 <img src={arrow} alt="" />
               </div>
             </button>
