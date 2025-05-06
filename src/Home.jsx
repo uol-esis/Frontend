16ac9e63f851642043a1c92a763bef7386e5baa8
Riphios <christian.janssen1@uni-oldenburg.de>
2025-05-06 11:10:59 +0200

    finished. pics should be bigger.

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,42 +6,40 @@ function Home() {
 
   return (
     <div className='flex justify-around items-around h-[85vh] bg-[#FEFAFA]'>
-      <div className='flex flex-col justify-start w-[35vw] h-[70vh] mt-[5vh] divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow'>
-          <div className="px-4 py-5 sm:p-6">
-            <img style={{ height: "192px", width: "100%", objectFit: "contain" }} src="/UploadPic.png" alt="Logo" />
-          </div>
-          <div className="flex flex-col justify-start gap-4 p-4">
-            <p className='text-xl font-semibold'>Daten hochladen</p>
-            <p>Excel Datein auswählen, anpassen und in die Datenbank hochladen.</p>
-            <button
-              type="button"
-              className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={() => navigate("/upload")}
-            >
-              Daten hochladen
-            </button>
-          </div>
+      <div className='flex flex-col justify-start w-[35vw] h-[70vh] mt-[5vh] divide-y divide-gray-200 rounded-lg bg-white shadow'>
+        <div className="flex h-55% justify-center items-center">
+          <img style={{ height: "auto", width: "60%", objectFit: "contain" }} src="/UploadPic.png" alt="Logo" />
+        </div>
+        <div className="flex flex-col justify-start h-45% p-4">
+          <p className='text-xl font-semibold'>Daten hochladen</p>
+          <p>Excel Datein auswählen, anpassen und in die Datenbank hochladen.</p>
+          <button
+            type="button"
+            className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={() => navigate("/upload")}
+          >
+            Daten hochladen
+          </button>
+        </div>
       </div>
 
-      <div className='flex flex-col justify-start w-[35vw] h-[70vh] mt-[5vh]'>
-        <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
-          <div className="px-4 py-5 sm:p-6">
-            <img style={{ height: "192px", width: "100%", objectFit: "contain" }} src="/DataVisPic.png" alt="Logo" />
-          </div>
-          <div className="flex flex-col justify-start gap-4 p-4">
-            <p className='text-xl font-semibold'>Datenvisualisierung</p>
-            <p>Daten aus der Datenbank zu Diagrammen und Statistiken aufbereiten.</p>
-            <button
-              type="button"
-              className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={() => window.location.href = import.meta.env.VITE_METABASE_ENDPOINT}
-            >
-              <div className="flex items-center justify-center g-3">
-                Visualisierung
-                <img src={arrow} alt="" />
-              </div>
-            </button>
-          </div>
+      <div className='flex flex-col justify-start w-[35vw] h-[70vh] mt-[5vh] divide-y divide-gray-200 rounded-lg bg-white shadow'>
+        <div className="flex h-55% justify-center items-center">
+          <img style={{ height: "auto", width: "60%", objectFit: "contain" }} src="/DataVisPic.png" alt="Logo" />
+        </div>
+        <div className="flex flex-col justify-start h-45% p-4">
+          <p className='text-xl font-semibold'>Datenvisualisierung</p>
+          <p>Daten aus der Datenbank zu Diagrammen und Statistiken aufbereiten.</p>
+          <button
+            type="button"
+            className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={() => window.location.href = import.meta.env.VITE_METABASE_ENDPOINT}
+          >
+            <div className="flex items-center justify-center g-3">
+              Visualisierung
+              <img src={arrow} alt="" />
+            </div>
+          </button>
         </div>
       </div>
     </div>
