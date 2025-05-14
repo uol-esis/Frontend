866624b7d278dc274e0e41cf657f6e03f79d5a25
Teetje Hillmann <hillmann@uni-bremen.de>
2025-05-14 20:46:51 +0200

    Speichern <-> Bearbeitung und editing state

--- src/components/ConverterCard.jsx
+++ src/components/ConverterCard.jsx
@@ -5,16 +5,41 @@ export default function ConverterCard({id, parameters}) {
     
     const [formData, setFormData] = useState({});
     
-    const handleInputChange = (param, value) => { //bisher sind die Parameter noch nicht kontrolliert im Hinblick auf required
+    const [errors, setErrors] = useState({}); //Fehlerstate
+
+    const [isEditing, setIsEditing] = useState(true); //State dient dafür dass man wenn man auf Speichern klickt, die Felder nicht mehr editierbar sind (hier müsste User ja auf Bearbeiten, Löschen oder so klicken, damit du die Chain korrekt speichern kannst)
+
+    const handleInputChange = (param, value, type) => { //bisher sind die Parameter noch nicht kontrolliert im Hinblick auf required
+        let error = "";
+        
+
         setFormData((prevData) => ({
             ...prevData,
             [param]: value,
         }));
+
+        setErrors((prevErrors) => ({
+            ...prevErrors,
+            [param]: error,
+        }));
     };
 
     const handleSave = () => {
-        // API MAGIC
-        console.log("Gespeicherte Daten:", formData);
+        let newErrors = {};
+
+        parameters.forEach(param => {
+            const value = formData[param.name];
+            if (param.required && (!value || value.toString().trim() === '')) {
+            newErrors[param.name] = 'Dieses Feld ist erforderlich.';
+            } 
+        });
+
+        setErrors(newErrors);
+        setIsEditing(false); //Editing State wird auf false gesetzt
+
+
+        // API MAGIC!!!!!!!!!!!!!! 
+        console.log("Gespeicherte Daten:", JSON.stringify(formData, null, 2));
     }
 
     return(
@@ -31,16 +56,33 @@ export default function ConverterCard({id, parameters}) {
                         type={param.type === 'number' ? 'number' : 'text'}
                         required={!!param.required}
                         value={formData[param.name] || ''}
-                        onChange={e => handleInputChange(param.name, e.target.value)}
-                        className="border border-gray-300 shadow rounded px-2 py-1 text-sm"
+                        onChange={e => handleInputChange(param.name, e.target.value, param.type)}
+                        readOnly={!isEditing} //man kann nur bearbeiten wenn Editing State true
+                        className={`border border-gray-300 shadow rounded px-2 py-1 text-sm 
+                            ${!isEditing ? 'bg-gray-100 text-gray-500 cursor-not-allowed' : 'bg-white'}`}
                     />
+                    {errors[param.name]&& (
+                            <p className="text-red-500 text-xs mt-1">{errors[param.name]}</p>
+                        )}
                     </div>
                 ))}
                 </div>
-            <button
-                className="absolute bottom-2 right-2 text-xs bg-gray-600 hover:bg-indigo-500 text-white rounded px-4 py-2"
-                onClick={handleSave}
-                >Speichern</button>
+                {isEditing ? ( //wenn editing false, dann kein Speichern
+                    <button
+                        className="absolute bottom-2 right-2 text-xs bg-blue-600 hover:bg-blue-700 text-white rounded px-4 py-2"
+                        onClick={handleSave}
+                    >
+                        Speichern
+                    </button>
+                    ) : (
+                    <button
+                        className="absolute bottom-2 right-2 text-xs bg-gray-600 hover:bg-indigo-500 text-white rounded px-4 py-2"
+                        onClick={() => setIsEditing(true)}
+                    >
+                        Bearbeiten
+                    </button>
+                    )}
+               
         </div>
     );
 }
