0ea83732f37a14157c73a1dfd0f6eff8a16f690b
Jonas Pohl <jonas.jp4c@gmail.com>
2025-06-28 14:32:03 +0200

    Remove public prefix from some images

--- src/Popups/ErrorDialog.jsx
+++ src/Popups/ErrorDialog.jsx
@@ -1,17 +1,17 @@
-export default function ErrorDialog({dialogRef, text, onConfirm, errorMsg}){
-    return(
-        <dialog  className=" justify-self-center p-5 mt-[20vh] w-[30vw] shadow-md bg-white " ref={dialogRef}>
+export default function ErrorDialog({ dialogRef, text, onConfirm, errorMsg }) {
+    return (
+        <dialog className=" justify-self-center p-5 mt-[20vh] w-[30vw] shadow-md bg-white " ref={dialogRef}>
             <div className="flex flex-col place-items-center p-5 gap-5 bg-white">
-                <img className="h-[15vh] w-[15vw] object-contain" src="public\error.jpg" alt="Upload fehlgeschlagen" />
+                <img className="h-[15vh] w-[15vw] object-contain" src="error.jpg" alt="Upload fehlgeschlagen" />
                 <p className="text-base font-semibold">{text}</p>
                 <p>{errorMsg}</p>
             </div>
             <button
-                    className=" p-5 mt-3 w-[15vw] rounded-md bg-gray-600 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    onClick={onConfirm}
-                >
-                    Ok
-                </button>
+                className=" p-5 mt-3 w-[15vw] rounded-md bg-gray-600 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={onConfirm}
+            >
+                Ok
+            </button>
         </dialog>
     );
 }
