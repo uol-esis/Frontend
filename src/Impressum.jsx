78d45d6a7603943f9f57f48e37395417d4b1805f
Teetje Hillmann <hillmann@uni-bremen.de>
2025-08-12 10:36:26 +0200

    Datenschutz und Impressum imhaltlich ausgebessert

--- src/Impressum.jsx
+++ src/Impressum.jsx
@@ -6,32 +6,41 @@ export default function Impressum() {
         <h1 className="text-3xl font-bold mb-6 text-gray-800">Impressum</h1>
 
         <section className="mb-6">
-          <h2 className="text-xl font-semibold text-gray-700 mb-2">Angaben gemäß § 5 TMG</h2>
+          <h2 className="text-xl font-semibold text-gray-700 mb-2">Beauftragter im datenschutzrechtlichen Sinne</h2>
           <p className="text-gray-700 leading-relaxed">
-            Max Mustermann<br />
-            Musterstraße 1<br />
-            12345 Musterstadt
+            Carl von Ossietzky Universität Oldenburg<br />
+            Ammerländer Heerstraße 114-118 <br />
+            26129 Oldenburg<br />
+            Telefon: 0441/7980<br />
+            E-Mail: infopoint@uol.de <br />
+            Vertreten durch Ihren Präsidenten: Prof. Dr. Ralph Bruder
           </p>
         </section>
 
         <section className="mb-6">
-          <h2 className="text-xl font-semibold text-gray-700 mb-2">Kontakt</h2>
+          <h2 className="text-xl font-semibold text-gray-700 mb-2">Datenschutzbeauftragter</h2>
           <p className="text-gray-700 leading-relaxed">
-            Telefon: 01234 567890<br />
-            E-Mail: <a href="mailto:max@mustermann.de" className="text-blue-600 hover:underline">max@mustermann.de</a>
+            Carl von Ossietzky Universität Oldenburg <br />
+            - Der Datenschutzbeauftragte - <br />
+            Ammerländer Heerstraße 114-118 <br />
+            26129 Oldenburg <br />
+            Telefon: 0441/ 7984196 <br />
+            E-Mail: dsuni@uol.de <br />
+            Internetauftritt: https://uol.de/datenschutz
           </p>
         </section>
 
         <section className="mb-6">
-          <h2 className="text-xl font-semibold text-gray-700 mb-2">Verantwortlich für den Inhalt nach § 18 Abs. 2 MStV</h2>
+          <h2 className="text-xl font-semibold text-gray-700 mb-2">Verantwortlicher des Mastergruppenprojekts "Döner"</h2>
           <p className="text-gray-700 leading-relaxed">
-            Max Mustermann<br />
-            Musterstraße 1<br />
-            12345 Musterstadt
+              Prof. Dr. Philipp Staudt <br />
+              Lehrstuhl für Wirtschaftsinformatik – Umwelt & Nachhaltigkeit <br />
+              Carl von Ossietzky Universität Oldenburg <br />
+              Ammerländer Heerstraße 114-118 <br />
+              26129 Oldenburg <br />
+              Mail: philipp.staudt@uni-oldenburg.de
           </p>
-        </section>
-
-        
+        </section>        
       </div>
     </main>
   );
