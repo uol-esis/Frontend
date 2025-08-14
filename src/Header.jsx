156cecaf62a93816b0be188eb7fb15cc6fa51a0d
Pascal Hiller <pascal.hiller@uni-oldenburg.de>
2025-08-14 11:17:11 +0200

    redirect to landingpage if logout

--- src/Header.jsx
+++ src/Header.jsx
@@ -108,7 +108,9 @@ export default function Header() {
           
 
           <button
-            onClick={() => { keycloak.logout(); window.location.href = "/"; }}
+            onClick={() => { keycloak.logout({
+                redirectUri: window.location.origin
+              }); }}
             className=" p-5 rounded-md bg-gray-600 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Logout
