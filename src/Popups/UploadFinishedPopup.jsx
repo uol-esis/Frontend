3f4a149ba4849804abc887e0798d6f352dc5fd10
Teetje Hillmann <hillmann@uni-bremen.de>
2025-09-11 14:03:56 +0200

    buttons gelöscht

--- src/Popups/UploadFinishedPopup.jsx
+++ src/Popups/UploadFinishedPopup.jsx
@@ -8,20 +8,6 @@ export default function UploadFinishedPopup({dialogRef}){
             <div className="flex flex-col place-items-center p-5 gap-5 bg-white">
                 <img className="h-[15vh] w-[15vw] object-contain" src="greenHook.jpg" alt="Upload finished" />
                 <p>The data was uploaded successfully</p>
-                <button
-                    className=" p-5 w-[15vw] rounded-md bg-gray-600 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    onClick={() => {navigate("/upload")}}
-                >
-                    Upload another file
-                </button>
-
-                <button
-                    className=" p-5 w-[15vw] rounded-md bg-gray-600 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    onClick={() => {navigate("/")}}
-                >
-                    Back to homepage
-                </button>
-                
             </div>
             
         </dialog>
