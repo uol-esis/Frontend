20294429bab72933b459dd52148e53cec8bb7f53
Teetje Hillmann <hillmann@uni-bremen.de>
2025-09-18 11:56:54 +0200

    Fehler kommt wieder

--- src/Popups/ConfirmNameDialog.jsx
+++ src/Popups/ConfirmNameDialog.jsx
@@ -16,7 +16,6 @@ export default function ConfirmNameDialog({dialogRef, name, onClickFunction,  er
         setTableTooLong(getByteSize(text) > 255);
     }, [text]);
 
-
         useEffect(() => {
             setText(name);
         }, [name]);
@@ -46,6 +45,7 @@ export default function ConfirmNameDialog({dialogRef, name, onClickFunction,  er
         if (typeof onClickFunction === "function") {
             const success = await onClickFunction(newFileName, text);
             if(!success) {
+                setError("Name wird bereits verwendet");
                 return;
             }
         }
@@ -84,7 +84,7 @@ export default function ConfirmNameDialog({dialogRef, name, onClickFunction,  er
                             name="fileName"
                             type="text"
                             value={baseName}
-                            onChange={(evt) => setBaseName(evt.target.value)} // Update base name only
+                            onChange={(evt) => setBaseName(evt.target.value)} 
                             className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm"
                         />
                         <span className="text-gray-500 pr-2">{extension}</span> {/* Display extension */}
@@ -104,14 +104,17 @@ export default function ConfirmNameDialog({dialogRef, name, onClickFunction,  er
                     <label htmlFor="username" className=" text-left block text-sm/6 font-medium text-gray-900">
                         Generierter Tabellentransformationsname:
                     </label>
-                    <p className="text-sm font-semibold text-red-800 bg-red-200 mb-1">{errorText}</p>
+                    <p className="text-sm font-semibold text-red-800 bg-red-200 mb-1">{error}</p>
                         <div className="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                         <input
                             id="username"
                             name="dataName"
                             type="text"
                             value={text}
-                            onChange={(evt) => { setText(evt.target.value); }}
+                            onChange={(evt) => { 
+                                setText(evt.target.value);
+                                setError("");
+                            }}
                             className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
                         />
                         </div>
@@ -126,9 +129,9 @@ export default function ConfirmNameDialog({dialogRef, name, onClickFunction,  er
                     </button>
                     <button
                         onClick={handleConfirm}
-                        disabled={tooLong || tableTooLong || errorText}
+                        disabled={tooLong || tableTooLong || error}
                         className={`p-5 rounded-md py-2 text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600
-                        ${tooLong || tableTooLong || errorText ? "bg-gray-300 text-gray-500 cursor-not-allowed" : "bg-gray-600 text-white hover:bg-indigo-500"}`} >
+                        ${tooLong || tableTooLong || error ? "bg-gray-300 text-gray-500 cursor-not-allowed" : "bg-gray-600 text-white hover:bg-indigo-500"}`} >
                         Bestätigen
                     </button>
                 </div>
